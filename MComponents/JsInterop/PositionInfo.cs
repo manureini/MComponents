fd9580f957d49f7057483551808d57b94cebc35a
manureini <manureini@users.noreply.github.com>
2020-01-04 00:01:15 +0100

    Old chrome v 46 does not have all values - but don't need them yet

--- MComponents/JsInterop/PositionInfo.cs
+++ MComponents/JsInterop/PositionInfo.cs
@@ -6,24 +6,24 @@
 
         public double left { get; set; }
 
-        public double right { get; set; }
+        public double? right { get; set; }
 
-        public double bottom { get; set; }
+        public double? bottom { get; set; }
 
-        public double abstop { get; set; }
+        public double? abstop { get; set; }
 
-        public double absleft { get; set; }
+        public double? absleft { get; set; }
 
-        public double absright { get; set; }
+        public double? absright { get; set; }
 
-        public double absbottom { get; set; }
+        public double? absbottom { get; set; }
 
-        public double height { get; set; }
+        public double? height { get; set; }
 
-        public double width { get; set; }
+        public double? width { get; set; }
 
-        public double x { get; set; }
+        public double? x { get; set; }
 
-        public double y { get; set; }
+        public double? y { get; set; }
     }
 }
