7097bd018efefff3e79d39e93e1fe12af8be45db
manureini <manureini@users.noreply.github.com>
2020-08-28 20:18:45 +0200

    Set time to 00:00 when on focus

--- MComponents/MForm/InputElements/InputTime.cs
+++ MComponents/MForm/InputElements/InputTime.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 
 namespace MComponents.InputElements
@@ -9,13 +10,14 @@ namespace MComponents.InputElements
     {
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            builder.OpenElement(0, "input");
-            builder.AddMultipleAttributes(1, AdditionalAttributes);
-            builder.AddAttribute(2, "class", CssClass);
-            builder.AddAttribute(2, "type", "time");
-            builder.AddAttribute(3, "value", FormatValueAsString(CurrentValue)); //custom format
-            builder.AddAttribute(4, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
-            builder.CloseElement();
+            builder.OpenElement(12, "input");
+            builder.AddMultipleAttributes(13, AdditionalAttributes);
+            builder.AddAttribute(14, "class", CssClass);
+            builder.AddAttribute(15, "type", "time");
+            builder.AddAttribute(16, "value", FormatValueAsString(CurrentValue)); //custom format
+            builder.AddAttribute(17, "onchange", EventCallback.Factory.CreateBinder<string>(this, pValue => CurrentValueAsString = pValue, CurrentValueAsString));
+            builder.AddAttribute(18, "onfocus", EventCallback.Factory.Create<FocusEventArgs>(this, _ => SetDefaultTimeIfNull()));
+            builder.CloseElement();         
         }
 
         protected override bool TryParseValueFromString(string value, out T result, out string validationErrorMessage)
@@ -28,19 +30,31 @@ namespace MComponents.InputElements
                 return false;
             }
 
-            object datetime = DateTime.Parse(value);
+            if(DateTime.TryParse(value, out var datetime))
+            {
+                result = (T)((object)datetime);
+                return true;
+            }
 
-            result = (T)datetime;
-            return true;
+            result = default;
+            return false;
         }
 
         protected override string FormatValueAsString(T value)
         {
             if (value == null)
-                return null; 
+                return null;
 
             dynamic v = value;
             return v.ToString("HH:mm");
+        }
+
+        protected void SetDefaultTimeIfNull()
+        {
+            if (string.IsNullOrEmpty(CurrentValueAsString))
+            {
+                CurrentValueAsString = "00:00";
+            }
         }
     }
 
