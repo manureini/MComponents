88b4b0ce5b1086fe41bc69b168e24f6c6516c1cf
manureini <manureini@users.noreply.github.com>
2020-10-04 11:11:38 +0200

    revert to datetime-local

--- MComponents/MForm/InputElements/InputDateTime.cs
+++ MComponents/MForm/InputElements/InputDateTime.cs
@@ -10,7 +10,6 @@ namespace MComponents.InputElements
         //datetime-local is not supported in firefox - maybe in 10 years or something.....
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            /*
             builder.OpenElement(0, "input");
             builder.AddMultipleAttributes(1, AdditionalAttributes);
             builder.AddAttribute(2, "class", CssClass);
@@ -18,7 +17,8 @@ namespace MComponents.InputElements
             builder.AddAttribute(3, "value", FormatValueAsString(CurrentValue)); //custom format
             builder.AddAttribute(4, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
             builder.CloseElement();
-            */
+
+            /*
 
             builder.OpenElement(0, "div");
             builder.AddAttribute(24, "class", "m-combined-input-container " + CssClass);
@@ -40,11 +40,11 @@ namespace MComponents.InputElements
             builder.CloseComponent();
 
             builder.CloseElement(); //div
+            */
         }
 
         protected override bool TryParseValueFromString(string value, out T result, out string validationErrorMessage)
         {
-            /*
             validationErrorMessage = null;
 
             if (value == null || value == string.Empty)
@@ -61,19 +61,15 @@ namespace MComponents.InputElements
 
             result = (T)(object)datetime;
             return true;
-            */
-            throw new NotImplementedException();
         }
 
         protected override string FormatValueAsString(T value)
-        {/*
+        {
             if (value == null)
                 return null;
 
             dynamic v = value;
             return v.ToString("yyyy-MM-ddTHH:mm");
-            */
-            throw new NotImplementedException();
         }
     }
 }
